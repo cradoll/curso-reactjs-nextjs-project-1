c838c8dc1a19a20c0a8add130f88d58ae69df5c8
Christian Radoll da Silva <christianradoll@gmail.com>
2021-05-15 10:54:44 -0300

    Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Component } from "react";
 
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -21,12 +22,42 @@ class App extends Component {
       },
     ],
   };
+  timeoutUpdate = null;
+
+  //Componente Montado
+  componentDidMount() {
+    //Utilizado para chamar a API
+    this.handleTimeout();
+  }
+
+  //Componente Atualizado
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  //Componente será desmontado
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "O título mudou";
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 5000);
+    console.log(posts[0].title);
+  };
 
   render() {
-    const { posts } = this.state;
+    //Chamado novamente quando um estado é atualizado
+    const { posts, counter } = this.state;
+
     //return <h1>Oi</h1>;
     return (
       <div className="App">
+        <h1>{counter} </h1>
         {posts.map((post) => (
           <div key={post.id}>
             <h1>{post.title}</h1>
